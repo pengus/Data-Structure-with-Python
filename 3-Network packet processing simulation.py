b20a86415c5586bedbb822365ad4c6ef83f4b0a1
pengus <36895141+pengus@users.noreply.github.com>
2020-04-28 03:26:47 +0300

    Add files via upload

---
+++ 3-Network packet processing simulation.py
@@ -0,0 +1,57 @@
+
+# python3
+
+from collections import namedtuple
+
+Request = namedtuple("Request", ["arrived_at", "time_to_process"])
+Response = namedtuple("Response", ["was_dropped", "started_at"])
+
+
+class Buffer:
+    def __init__(self, size):
+        self.size = size
+        self.finish_time = []
+
+    def process(self, request):
+        # write your code here
+        if len(self.finish_time) == 0:
+            self.finish_time.append(request.arrived_at+request.time_to_process)
+            return Response(False, request.arrived_at)
+        elif len(self.finish_time) < self.size :
+            
+            self.finish_time.append(max(self.finish_time[-1]+request.time_to_process,request.time_to_process+request.arrived_at))
+            return Response(False, max(self.finish_time[-2],request.arrived_at) )
+        else:
+            if not (self.finish_time[0] > request.arrived_at):
+                self.finish_time.append(max(self.finish_time[-1]+request.time_to_process,request.time_to_process+request.arrived_at))
+
+                self.finish_time.pop(0)
+                return Response(False, self.finish_time[-1]-request.time_to_process )
+                  
+
+        return Response(True, -1)
+
+
+def process_requests(requests, buffer):
+    responses = []
+    for request in requests:
+        responses.append(buffer.process(request))
+    return responses
+
+
+def main():
+    buffer_size, n_requests = map(int, input().split())
+    requests = []
+    for _ in range(n_requests):
+        arrived_at, time_to_process = map(int, input().split())
+        requests.append(Request(arrived_at, time_to_process))
+
+    buffer = Buffer(buffer_size)
+    responses = process_requests(requests, buffer)
+
+    for response in responses:
+        print(response.started_at if not response.was_dropped else -1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 5-Maximum in Sliding Window.py
@@ -0,0 +1,131 @@
+
+class Element():
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+class Sequence():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def print_(self):
+        init = self.head
+        printer =[]
+        while init != None:
+            printer.append(init.value)
+            init = init.next
+        print(printer)
+        return printer
+
+    def append_ (self, value):
+        
+        if self.head != None:
+            node = Element(value)
+            node.prev = self.tail
+            self.tail.next = node
+            
+            self.tail = node 
+        else: 
+            node = Element(value)
+            self.head = node
+            self.tail = node
+
+    def leftappend_(self, value):
+        if self.head != None:
+            node = Element(value)
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        else: 
+            node = Element(value,None)
+            self.head = node
+            self.tail = node
+
+    def pop_ (self):
+        if self.tail.prev != None:
+            self.tail.prev.next = None
+        else:
+           self.head = None
+        self.tail = self.tail.prev
+
+    def leftpop_(self):        
+        if self.head.next != None:
+            self.head = self.head.next
+            self.head.prev = None
+        else:
+            self.tail = None
+            self.head = None
+        
+      
+    def is_not_empty(self):
+        if self.head == None :
+            return False 
+        return True
+
+    def give_item(self, index):
+
+        if index >= 0 :
+            counter = 1
+            temp = self.head   
+            while self.is_not_empty() and counter <= index:
+                temp = temp.next
+                counter += 1
+            return temp.value
+        else:
+            temp = self.tail
+            counter = -1
+            while self.is_not_empty() and counter > index:
+                temp = temp.prev
+                counter -=1
+            return temp.value
+
+def max_sliding_window(sequence, m):
+    maximums = []
+    deq = Sequence()
+    for i in range(m):
+
+        while deq.is_not_empty() and sequence[deq.give_item(-1)] <= sequence[i]:
+            deq.pop_()
+        deq.append_(i)
+
+    for j in range(m,len(sequence)):
+        print(str(sequence[deq.give_item(0)]) + " ", end = "")
+
+        while deq.is_not_empty() and deq.give_item(0) < j-i:
+            deq.leftpop_()
+        while deq.is_not_empty() and sequence[deq.give_item(-1)] <= sequence[j]:
+            deq.pop_()
+        deq.append_(j)
+    print(str(sequence[deq.give_item(0)]))
+
+        
+
+if __name__ == '__main__':
+    n = int(input())
+    input_sequence = [int(i) for i in input().split()]
+    assert len(input_sequence) == n
+    window_size = int(input())
+    max_sliding_window(input_sequence,window_size)
+
+
+    #s = Sequence()
+    #s.append_(1)
+    #s.leftpop_()
+    ##s.append_(1)
+    ##s.append_(1)
+    ##s.append_(1)
+    ##s.append_(1)
+    ##s.append_(1)
+    ##s.append_(1)
+    ##s.print_()
+    ##s.pop_()
+    ##s.pop_()
+    #s.print_()
+    ##s.pop_()
+    ##s.pop_()
+    ##s.pop_()
+    ##s.pop_()
+
+
